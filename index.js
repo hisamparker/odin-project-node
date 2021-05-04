22bd6c263278197a7dc8df3d30e2ac97a30bbfe2
sam <hisamparker@gmail.com>
2021-05-04 19:07:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        fs.createReadStream('index.html').pipe(res);
+    } else if(req.url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        fs.createReadStream('about.html').pipe(res);
+    } else if(req.url === '/contact-me') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        fs.createReadStream('contact-me.html').pipe(res);
+    } else {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        fs.createReadStream('404.html').pipe(res);
+    } 
+});
+
+server.listen(8080);
